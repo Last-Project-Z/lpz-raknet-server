a8055cba3c5de16e129dae41f98a8058f9cfe568
Burak <pro.daggermen@gmail.com>
2021-01-18 16:36:46 +0300

    Update precompiler.h

--- lpz-raknet-server/src/precompiler.h
+++ lpz-raknet-server/src/precompiler.h
@@ -1,12 +1,6 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
-#include <string.h>
-
 #define HAVE_STDINT_H
 
 #include "SDK/amx/amx.h"
 #include "SDK/plugincommon.h"
-
-using namespace std;
